2e2e167809b4b245d1e1140f79381e7e3640345c
André Andersson <andre.eric.andersson@gmail.com>
2021-01-10 20:04:18 +0100

    Update release version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/Qluxzz/avanza'
 EMAIL = 'andre.eric.andersson@gmail.com'
 AUTHOR = 'André Andersson'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '2.3.0'
+VERSION = '2.4.0'
 
 REQUIRED = [
     'requests>=2',
