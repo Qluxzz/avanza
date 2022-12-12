322ab38e1ad100d786b6ee5f740aafc6404a52c1
tux2000 <tux2000@users.noreply.github.com>
2022-12-12 18:10:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/Qluxzz/avanza'
 EMAIL = 'andre.eric.andersson@gmail.com'
 AUTHOR = 'André Andersson'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '4.0.2'
+VERSION = '5.0.0'
 
 REQUIRED = [
     'requests>=2',
