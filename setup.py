c9b0934e27d1b1b0f5adcabf437fcb83ff768d12
André Andersson <andre.eric.andersson@gmail.com>
2023-03-16 10:48:35 +0100

    Bump minor version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/Qluxzz/avanza'
 EMAIL = 'andre.eric.andersson@gmail.com'
 AUTHOR = 'André Andersson'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '7.0.0'
+VERSION = '7.1.0'
 
 REQUIRED = [
     'requests>=2',
