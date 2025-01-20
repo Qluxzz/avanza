86b72421a80820cd221fa063621de071e679c383
Qluxzz <andre.eric.andersson@gmail.com>
2025-01-20 10:10:16 +0100

    Update response model

--- avanza/models/watch_list.py
+++ avanza/models/watch_list.py
@@ -2,8 +2,21 @@ from typing import List
 from pydantic import BaseModel
 
 
+class CustomerId(BaseModel):
+    id: int
+
+
+class UserId(BaseModel):
+    customerId: CustomerId
+
+
 class WatchList(BaseModel):
-    editable: bool
-    id: str
+    watchListId: str
+    userId: UserId
+    orderbookIds: List[str]
+    created: str
+    """ ISO 8601 """
+    modified: str
+    """ ISO 8601 """
     name: str
-    orderbooks: List[str]
+    urlName: str
