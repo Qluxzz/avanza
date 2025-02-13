7259be0f95edbf21411a70184e34f81ef95b5d3c
André Andersson <andre.eric.andersson@gmail.com>
2025-02-13 20:42:58 +0100

    Bump major version due to changed call to edit order

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/Qluxzz/avanza"
 EMAIL = "andre.eric.andersson@gmail.com"
 AUTHOR = "André Andersson"
 REQUIRES_PYTHON = ">=3.9.0"
-VERSION = "14.0.0"
+VERSION = "15.0.0"
 
 REQUIRED = ["requests>=2", "pyotp>=2", "websockets>=8", "pydantic>=2"]
 
