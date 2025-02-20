67ed9ba6add2d25cf1372165c61fdae2f9684626
Qluxzz <andre.eric.andersson@gmail.com>
2025-02-20 21:05:55 +0100

    Bump required version to 11

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ AUTHOR = "André Andersson"
 REQUIRES_PYTHON = ">=3.9.0"
 VERSION = "15.0.0"
 
-REQUIRED = ["requests>=2", "pyotp>=2", "websockets>=8", "pydantic>=2"]
+REQUIRED = ["requests>=2", "pyotp>=2", "websockets>=11", "pydantic>=2"]
 
 EXTRAS = {}
 
