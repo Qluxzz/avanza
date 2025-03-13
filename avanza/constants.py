1101d788aead51df87b62785e5aaaf6a06c32f11
Unt3r <151835497+Unt3r@users.noreply.github.com>
2025-03-13 19:46:55 +0100

    Update constants.py
    Added route to ORDERBOOK_PATH

--- avanza/constants.py
+++ avanza/constants.py
@@ -154,6 +154,7 @@ class Route(enum.Enum):
     ORDER_PLACE_PATH_BUY_FUND = "/_api/fund-guide/fund-order-page/buy"
     ORDER_PLACE_PATH_SELL_FUND = "/_api/fund-guide/fund-order-page/sell"
     ORDER_EDIT_PATH = "/_api/trading-critical/rest/order/modify"
+    ORDERBOOK_PATH = '/_api/trading-critical/rest/orderbook/{}'
     ORDERBOOK_LIST_PATH = "/_mobile/market/orderbooklist/{}"
     PRICE_ALERT_PATH = "/_cqbe/marketing/service/alert/{}"
     STOP_LOSS_PATH = "/_api/trading/stoploss"
